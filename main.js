f475b33029c3592fad8d247eb4eaa7e580ab3959
Michał <48693761+mbro89@users.noreply.github.com>
2019-03-27 18:50:03 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+var navBtn = document.querySelector("nav");
+
+navBtn.addEventListener("click", function(event){
+  navBtn.classList.toggle("nav-btn-active");
+});
+
+
+
+
+
+});
